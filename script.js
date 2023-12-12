dd2bfc5f24a0a8c6605a7ea3db6f5ff1b46a9838
xRayz3n <sebastien_lam@hotmail.fr>
2023-12-12 19:11:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,131 @@
+// Define the character set used in your model training
+const chars = ['\n', ' ', '!', '$', '&', "'", ',', '-', '.', '3', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; // Add or remove characters as needed
+const stoi = {};
+chars.forEach((char, index) => stoi[char] = index);
+const itos = chars;
+const vocabSize = 65;
+const sequence_length = 64;
+// Encode and decode functions
+function encode(str) {
+    return str.split('').map(char => stoi[char]);
+}
+
+function softmax(arr) {
+    const maxLogit = Math.max(...arr);
+    const scaled = arr.map(logit => Math.exp(logit - maxLogit));
+    const total = scaled.reduce((acc, val) => acc + val, 0);
+    return scaled.map(val => val / total);
+}
+function sampleIndex(probabilities) {
+    const rnd = Math.random();
+    let cumSum = 0;
+    for (let i = 0; i < probabilities.length; i++) {
+        cumSum += probabilities[i];
+        if (rnd < cumSum) return i;
+    }
+    return probabilities.length - 1;
+}
+
+
+function decode(outputIndices) {
+    let text = '';
+
+    for (let i = 0; i < outputIndices.length; i += vocabSize) {
+        const logits = outputIndices.slice(i, i + vocabSize);
+        const probabilities = softmax(logits);
+        const randomIndex = weightedRandomSelect(probabilities);
+        text += itos[randomIndex];
+    }
+
+    return text[text.length-1];
+}
+
+function weightedRandomSelect(probabilities) {
+    let sum = 0;
+    const r = Math.random();
+    for (let i = 0; i < probabilities.length; i++) {
+        sum += probabilities[i];
+        if (r <= sum) return i;
+    }
+    return probabilities.length - 1; // Return the last index in case random number exceeds sum
+}
+
+
+// Global variable for the ONNX session
+let session;
+
+// Load the ONNX model using ONNX Runtime Web
+async function loadModel() {
+    session = await ort.InferenceSession.create("./gpt2.onnx");
+}
+
+// Function to generate text based on the user input
+async function generateText() {
+
+    if (!session) {
+        console.error('Model not loaded yet');
+        return;
+    }
+
+    let inputText = document.getElementById('inputText').value;
+    let maxLines = document.getElementById('lineInput').value;
+    if (!inputText) {
+        console.error('Input text is empty');
+        return;
+    }
+    let encodedInput = encode(inputText);
+
+    if (encodedInput.length === 0) {
+        console.error('Encoded input is empty');
+        return;
+    }
+
+    // Preprocess encodedInput to match the shape [64, 256]
+    // This might involve padding or truncating the input
+
+    // Create the input tensor with the correct shape
+    // Run the model
+    let generatedText = inputText;
+    let context = inputText;
+    let lines = 0;
+    try {
+        while (lines < maxLines) {
+        
+        let inputTensor = new ort.Tensor("int32", encode(context), [1, context.length]);
+        const outputMap = await session.run({ 'inputs': inputTensor });
+        const outputTensor = outputMap['outputs'];
+
+        const outputIndices = Array.from(outputTensor.data);
+        const generatedLetter = decode(outputIndices);
+        
+        result = inputText+generatedLetter;
+        inputText += generatedLetter;
+
+        context += generatedLetter;
+        if (context.length >= sequence_length) {
+            context = context.slice(1);
+        }
+
+        if (generatedLetter == '\n') {
+            lines+=1;
+            if (context[context.length-2] == '\n') {
+                lines-=1;
+            }
+        }
+
+        document.getElementById('outputText').innerText = result;
+        await new Promise(resolve => setTimeout(resolve, 10));
+        }
+
+    } catch (error) {
+        console.error('Error during model run:', error);
+    }
+}
+
+// Event listener for the Generate button
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('generateButton').addEventListener('click', generateText);
+});
+
+// Load the model immediately when the script is loaded
+loadModel();
---
+++ test-pt.py
@@ -0,0 +1,37 @@
+from gpt import GPTLanguageModel
+import torch
+
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+# with open('./dataset/processed_dataset.txt', 'r', encoding='utf-8') as f:
+#     text = f.read()
+
+# here are all the unique characters that occur in this text
+
+# chars = sorted(list(set(text)))
+
+chars = ['\n', ' ', '!', '$', '&', "'", ',', '-', '.', '3', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+vocab_size = len(chars)
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+model = GPTLanguageModel().to('cuda')
+model.load_state_dict(torch.load('./gpt.pt'))
+model.eval()
+user_input = input("from what do you want to start ? ")
+
+context = torch.tensor([encode(user_input)], dtype=torch.int32, device='cuda')
+print(decode(model.generate(context, max_new_tokens=100)[0].tolist()))
+
+dummy_input = torch.zeros(1, 128, dtype=torch.int32).to("cuda")
+torch.onnx.export(model,
+                dummy_input,
+                "gpt2.onnx",
+                input_names=['inputs'],
+                output_names=['outputs'], 
+                verbose=False,
+                opset_version=13,
+                dynamic_axes={'inputs': {0: 'batch_size', 1: 'sequence_length'}, 'outputs': {0: 'batch_size', 1: 'sequence_length'}}
+)
